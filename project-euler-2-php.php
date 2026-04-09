fae675bd95b8192b023f6785d9458be9ed405294
AlexandrAnatoliev <per-1986@list.ru>
2026-04-09 16:15:46 +0300

    fix: replace header to include link

--- project-euler-2-php.php
+++ project-euler-2-php.php
@@ -11,25 +11,7 @@
   </head>
 
   <body>
-    <header>
-      <div class="favicon">
-        <a href="/"><img src="favicon.ico" alt="Проект Эйлера блог"
-                                           class="favicon"></a>
-      </div>
-      <div class="github">
-        <a href="https://github.com/AlexandrAnatoliev"><img
-           src="images/logo/github.svg" alt="Ссылка на github"
-                                        class="github"></a>
-      </div>
-
-      <nav>
-        <ul>
-          <li><a href="projects.php"><b>Projects</b></a></li>
-          <li><a href="index.php"><b>Blog</b></a></li>
-          <li><a href="project-euler-problems.php"><b>Problems</b></a></li>
-        </ul>
-      </nav>
-    </header>
+    <?php include 'includes/header.php'; ?>
 
     <section>
       <h1 id="top">Проект Эйлер 2 задача на PHP</h1>
