4aedfa5651e49645b0f9db4e4c4164f450352202
AlexandrAnatoliev <per-1986@list.ru>
2026-04-09 16:10:08 +0300

    fix: separate the header into file

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<header>
+  <div class="favicon">
+    <a href="/"><img src="favicon.ico" alt="Проект Эйлера блог"
+                                       class="favicon"></a>
+  </div>
+  <div class="github">
+    <a href="https://github.com/AlexandrAnatoliev"><img
+       src="images/logo/github.svg" alt="Ссылка на github"
+                                    class="github"></a>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="projects.php"><b>Projects</b></a></li>
+      <li><a href="index.php"><b>Blog</b></a></li>
+      <li><a href="project-euler-problems.php"><b>Problems</b></a></li>
+    </ul>
+  </nav>
+</header>
