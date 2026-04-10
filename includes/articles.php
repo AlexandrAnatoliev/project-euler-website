843909e2a56045f3fd2e728ee6a95ce1fd52ac6c
AlexandrAnatoliev <per-1986@list.ru>
2026-04-10 16:44:07 +0300

    fix: add article array

---
+++ includes/articles.php
@@ -0,0 +1,13 @@
+<?php
+$article = [
+    ['aria_label'   =>  'article 1',
+    'header'        =>  'Выделение CSS-настроек в отдельный репозиторий',
+    'preview'       => 'Project-Euler-CSS - это CSS-настройки для моего блога Project-Euler-Blog.
+        Выделение CSS-настроек в отдельный репозиторий позволяет
+        использовать их повторно в разных проектах и избавляет от необходимости
+        вносить одни и те же изменения в каждый проект...',
+    'link'          => '<a href="vydelenie-css-v-otdelnyj-repozitorij.php"><b>читать</b></a>',
+    ],
+];
+echo $article[0]['link'];
+?>
